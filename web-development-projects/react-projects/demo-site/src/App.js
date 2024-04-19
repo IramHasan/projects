91ddd109b4c91d3c1a2731a20024f1e8e4d9cf19
IramHasan <75052702+IramHasan@users.noreply.github.com>
2024-04-19 02:35:42 -0400

    Demo React Site
    Created this demo react app in order to get a refresher for basic React principles.

---
+++ web-development-projects/react-projects/demo-site/src/App.js
@@ -0,0 +1,19 @@
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
+
+function App() {
+
+  const name = "Iram";
+  return (
+    <div className="App">
+
+      <Header title="Grocery List"/>
+      <Content/>
+      <Footer/>
+
+    </div>
+  );
+}
+
+export default App;
